a96cf4bd079aad0e91c7bf7f2dda7ee14bdcc0b5
unknown <hyangsu,shin@student.sl.on.ca>
2022-11-14 12:21:43 -0500

    Chapter 10 - Adding Damage Effects

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -11,16 +11,44 @@ public class Gun : MonoBehaviour
     public AudioClip liveFire;
     public AudioClip dryFire;
 
+    public float zoomFactor;// controls the zoom level when the player hits the right mouse button
+    public int range;       // how far the gun can effectively hit its target
+    public int damage;      // how much damage the gun causes.
+    private float zoomFOV;  // the field of view based on the zoom factor
+    private float zoomSpeed = 6;
+
     // Start is called before the first frame update
     void Start()
     {
+        zoomFOV = Constants.CameraDefaultZoom / zoomFactor;
         lastFireTime = Time.time - 10;  //could fire the gun immediately when the game start
     }
 
     // Update is called once per frame
     protected virtual void Update()
     {
-        
+        // Right Click (Zoom)
+        if (Input.GetMouseButton(1))    // hit right mouse button
+        {
+            Camera.main.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView,
+            zoomFOV, zoomSpeed * Time.deltaTime);
+        }
+        else
+        {
+            Camera.main.fieldOfView = Constants.CameraDefaultZoom;
+        }
+    }
+
+    private void processHit(GameObject hitObject)
+    {
+        if (hitObject.GetComponent<Player>() != null)
+        {
+            hitObject.GetComponent<Player>().TakeDamage(damage);
+        }
+        if (hitObject.GetComponent<Robot>() != null)
+        {
+            hitObject.GetComponent<Robot>().TakeDamage(damage);
+        }
     }
 
     protected void Fire()
@@ -35,5 +63,13 @@ public class Gun : MonoBehaviour
         GetComponent<AudioSource>().PlayOneShot(dryFire);
         }               
         GetComponentInChildren<Animator>().Play("Fire");
+
+        // This creates a ray and checks what that ray hits.
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, range))
+        {
+            processHit(hit.collider.gameObject);
+        }
     }
 }
